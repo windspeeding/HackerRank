411dbd6f4171f6da9aa46ba0d214b2f174614635
Feng Ding <windspeeding@gmail.com>
2016-04-16 18:21:33 -0700

    Java Intro Stdin and Stdout 1

---
+++ src/com/windspeeding/Java/Introduction/StdinAndStdout1.java
@@ -0,0 +1,19 @@
+package com.windspeeding.Java.Introduction;
+
+import java.util.Scanner;
+
+/**
+ * Created by Feng on 4/16/2016.
+ */
+public class StdinAndStdout1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();      //Complete this line
+        int c = sc.nextInt();      //Complete this line
+
+        System.out.println(a);
+        System.out.println(b);      //Complete this line
+        System.out.println(c);      //Complete this line
+    }
+}
