5931a48fe8b98a150cddd8f5c2fff67256917834
Feng Ding <windspeeding@gmail.com>
2016-04-16 18:05:35 -0700

    Java Intro

---
+++ src/com/windspeeding/Java/Introduction/WelcomeToJava.java
@@ -0,0 +1,12 @@
+package com.windspeeding.Java.Introduction;
+
+/**
+ * Created by Feng on 4/16/2016.
+ */
+public class WelcomeToJava {
+    public static void main(String[] args){
+        //Write your solution here.
+        System.out.println("Hello World.");
+        System.out.println("Hello Java.");
+    }
+}
